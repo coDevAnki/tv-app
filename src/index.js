e175d435605cc18bf479fc55c19cbd8f83be5e21
unknown <impratsbanerjee@gmail.com>
2021-02-09 10:43:24 +0530

    setup babel with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 let h = { webpack: "working" };
 console.log(h);
 console.log("hello");
+let d = { ...h, babel: "working on" };
+console.log("new ", d);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
 module.exports = {
-  mode: "development",
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
   devServer: {
     contentBase: "./dist",
   },
+
+  devtool: "source-map",
 };
